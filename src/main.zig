d247e90d87e074c3317464825facbb8ca6a3168f
eddineimad0 <zerrouq.imad@outlook.com>
2023-05-05 22:23:32 +0100

    Removed the tests in main.

--- src/main.zig
+++ src/main.zig
@@ -1,17 +1,4 @@
-const std = @import("std");
-const testing = std.testing;
-const geometry = @import("./core/geometry.zig");
-
-export fn add(a: i32, b: i32) i32 {
-    var size = geometry.WidowSize.init(a, a) catch {
-        return 0;
-    };
-    size.scale(@intToFloat(f64, b)) catch {
-        std.debug.print("Failed to scale the size by parameter {}", .{b});
-    };
-    return size.width;
-}
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 21);
+export fn add() i32 {
+    const result = 0;
+    return result;
 }
