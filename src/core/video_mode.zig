ef0a3cb066fc92a8b810b74692af87e146926bb5
eddineimad0 <zerrouq.imad@outlook.com>
2023-05-11 19:15:45 +0100

    Switched the best_match logic to eculidean distance.

--- src/core/video_mode.zig
+++ src/core/video_mode.zig
@@ -1,5 +1,6 @@
 const math = @import("std").math;
 const MAX_U32 = math.maxInt(u32);
+
 /// Holds the Display's current video mode
 pub const VideoMode = struct {
     width: i32,
@@ -9,12 +10,12 @@ pub const VideoMode = struct {
 
     const Self = @This();
 
-    pub fn init(width: i32, height: i32, color_depth: u8, frequency: u16) Self {
+    pub fn init(width: i32, height: i32, frequency: u16, color_depth: u8) Self {
         return Self{
             .width = width,
             .height = height,
-            .color_depth = color_depth,
             .frequency = frequency,
+            .color_depth = color_depth,
         };
     }
 
@@ -22,35 +23,36 @@ pub const VideoMode = struct {
         return (self.width == other.width and self.height == other.height and self.color_depth == other.color_depth and self.frequency == other.frequency);
     }
 
-    /// As implemented by [`GLFW`] library this function returns the
-    /// best closest video mode to the `desired_mode` from a slice of supported video modes.
+    /// this function returns the closest video mode to the `desired_mode`
+    /// from a slice of supported video modes.
     pub fn select_best_match(desired_mode: *const Self, modes: []const Self) *const Self {
         var ret_val = desired_mode;
         var size_diff: i32 = undefined;
-        var least_size_diff: u32 = MAX_U32;
-        var rate_diff: i32 = undefined;
-        var least_rate_diff: u32 = MAX_U32;
-        var color_diff: i32 = undefined;
-        var least_color_diff: u32 = MAX_U32;
         var width_diff: i32 = undefined;
         var height_diff: i32 = undefined;
+        var rate_diff: i32 = undefined;
+        var color_diff: i32 = undefined;
+        var least_distance: u32 = MAX_U32;
+        var current_distance: u32 = undefined;
 
-        for (modes) |mode| {
-            color_diff = math.absInt(@intCast(i32, mode.color_depth) - @intCast(i32, desired_mode.color_depth)) catch 0;
-            width_diff = desired_mode.width - mode.width;
+        for (modes) |*mode| {
+
+            // Euclidean distance.
+            color_diff = @intCast(i32, mode.color_depth) - @intCast(i32, desired_mode.color_depth);
+            color_diff *|= color_diff;
+            rate_diff = @intCast(i32, mode.frequency) - @intCast(i32, desired_mode.frequency);
+            rate_diff *|= rate_diff;
+            width_diff = mode.width - desired_mode.width;
             width_diff *|= width_diff;
-            height_diff = desired_mode.height - mode.height;
+            height_diff = mode.height - desired_mode.height;
             height_diff *|= height_diff;
             size_diff = width_diff +| height_diff;
-            rate_diff = math.absInt(@intCast(i32, mode.frequency) - @intCast(i32, desired_mode.frequency)) catch 0;
-
-            if (color_diff < least_color_diff or (color_diff == least_color_diff and size_diff < least_size_diff) or (color_diff == least_color_diff and size_diff == least_size_diff and rate_diff < least_rate_diff)) {
-                ret_val = &mode;
-                least_color_diff = @intCast(u32, color_diff);
-                least_size_diff = @intCast(u32, size_diff);
-                least_rate_diff = @intCast(u32, rate_diff);
+            current_distance = math.sqrt(@intCast(u32, color_diff + rate_diff + size_diff));
+            if (current_distance < least_distance) {
+                ret_val = mode;
+                least_distance = @intCast(u32, current_distance);
             }
         }
-        return desired_mode;
+        return ret_val;
     }
 };
