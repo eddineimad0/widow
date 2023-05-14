56d9e3554f90a4c854923a6faf8dcabc4bf9475b
eddineimad0 <zerrouq.imad@outlook.com>
2023-05-14 22:44:48 +0100

    Renamed main.zig to widow.zig.

---
+++ src/widow.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+const platform = @import("platform");
+
+const Widow = struct {
+    internals: platform.internals.Internals,
+    devices: platform.internals.PhysicalDevices,
+    allocator: std.mem.Allocator,
+    const Self = @This();
+    pub fn init(allocator: std.mem.Allocator) !Self {
+        var self: Self = undefined;
+        self.internals = try platform.internals.Internals.init(allocator);
+        errdefer self.internals.deinit();
+        self.devices = try platform.internals.PhysicalDevices.init(allocator);
+        self.allocator = allocator;
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.internals.deinit();
+        self.devices.deinit();
+    }
+};
+
+test "should init Widow" {
+    const testing = std.testing;
+    var cntxt = try Widow.init(testing.allocator);
+    defer cntxt.deinit();
+}
