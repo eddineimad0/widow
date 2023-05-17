56fd803e6ccaffa8d9636fb403a6740113a71809
eddineimad0 <zerrouq.imad@outlook.com>
2023-05-17 18:30:13 +0100

    modified build.zig.

--- build.zig
+++ build.zig
@@ -15,31 +15,43 @@ pub fn build(b: *std.Build) void {
     // common module
     const common = b.createModule(.{ .source_file = .{ .path = "src/common/common.zig" } });
 
-    var platform_module: *std.build.Module = undefined;
-    switch (target.getOs().tag) {
-        .windows => {
+    var platform_module: *std.build.Module = switch (target.getOs().tag) {
+        .windows => windows: {
             var win32api = b.createModule(.{
                 .source_file = .{ .path = "libs/zigwin32/win32.zig" },
             });
             var deps: [2]std.build.ModuleDependency = undefined;
             deps[0] = std.build.ModuleDependency{ .name = "common", .module = common };
             deps[1] = std.build.ModuleDependency{ .name = "win32", .module = win32api };
-            platform_module = b.createModule(.{ .source_file = .{ .path = "src/platform/win32/platform.zig" }, .dependencies = &deps });
+            break :windows b.createModule(.{ .source_file = .{ .path = "src/platform/win32/platform.zig" }, .dependencies = &deps });
         },
         else => {
             @panic("Unsupported platform");
         },
-    }
+    };
     lib.addModule("platform", platform_module);
 
     b.installArtifact(lib);
 
+    const test_step = b.step("test", "Run library tests");
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/widow.zig" },
+        .root_source_file = .{ .path = "src/window.zig" },
         .target = target,
         .optimize = optimize,
     });
     main_tests.addModule("platform", platform_module);
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    main_tests.addModule("common", common);
+    const run_main_test = b.addRunArtifact(main_tests);
+    test_step.dependOn(&run_main_test.step);
+
+    const example_step = b.step("example", "Compile example");
+    const example = b.addExecutable(.{
+        .name = "simple_window",
+        .root_source_file = .{ .path = "test/simple_window.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    example.linkLibrary(lib);
+    b.installArtifact(example);
+    example_step.dependOn(&example.step);
 }
