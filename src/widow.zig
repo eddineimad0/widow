f89f31255a7a4ce44b0bf118fe8cdcf35c1a40ad
eddineimad0 <zerrouq.imad@outlook.com>
2023-05-17 18:28:43 +0100

    Created the window struct.

--- src/widow.zig
+++ src/widow.zig
@@ -1,29 +1,33 @@
 const std = @import("std");
 const platform = @import("platform");
 
-const Widow = struct {
-    internals: platform.internals.Internals,
-    devices: platform.internals.PhysicalDevices,
-    allocator: std.mem.Allocator,
-    const Self = @This();
-    pub fn init(allocator: std.mem.Allocator) !Self {
-        var self: Self = undefined;
-        self.internals = try platform.internals.Internals.init();
-        errdefer self.internals.deinit();
-        self.devices = try platform.internals.PhysicalDevices.init(allocator);
-        self.allocator = allocator;
+pub const window = @import("./window");
 
-        return self;
+pub const WidowContext = struct {
+    internals: *platform.internals.Internals,
+    is_init: bool,
+    const Self = @This();
+
+    pub fn init(allocator: std.mem.Allocator) !Self {
+        return Self{
+            .internals = try platform.internals.Internals.create(allocator),
+            .is_init = true,
+        };
     }
 
     pub fn deinit(self: *Self) void {
-        self.internals.deinit();
-        self.devices.deinit();
+        self.internals.destroy();
+        self.is_init = false;
     }
 };
 
 test "should init Widow" {
     const testing = std.testing;
-    var cntxt = try Widow.init(testing.allocator);
-    defer cntxt.deinit();
+    var cntxt = try WidowContext.init(testing.allocator);
+    defer {
+        cntxt.deinit();
+        std.debug.print("Deinit success:{}\n", .{!cntxt.is_init});
+    }
+    std.debug.print("Init success:{}\n", .{cntxt.is_init});
+    std.debug.print("Widow Struct size:{}\n", .{@sizeOf(WidowContext)});
 }
