232052055f23ee8d7daccd6805b59caf48e385a9
eddineimad0 <zerrouq.imad@outlook.com>
2023-05-17 18:26:24 +0100

    Fixed the declaration.

---
+++ src/common/window_data.zig
@@ -0,0 +1,41 @@
+// use crate::platform::platform_impl;
+//     cursor::WidowCursor,
+//     input::InputState,
+//     types::{FullScreenMode, WidowPoint2D, WidowSize},
+
+const VideoMode = @import("./video_mode.zig").VideoMode;
+const WidowPoint2D = @import("./geometry.zig").WidowPoint2D;
+const WidowSize = @import("./geometry.zig").WidowSize;
+
+pub const WindowFlags = struct {
+    is_visible: bool, // Does the window appear on screen or is it hidden from the user.
+    is_maximized: bool, // Is the window maximized.
+    is_minimized: bool, // Is the window minimized.
+    is_resizable: bool, // Can the window be resized by draging the edges or maximzed.
+    is_decorated: bool, // Does the window have a title bar.
+    is_topmost: bool, // Should the window be always on top of all active windows.
+    is_focused: bool, // Does the window have keyboard and input focus.
+    cursor_in_client: bool, // Is the cursor currently in client area.
+    accepts_raw_input: bool,
+};
+
+pub const FullScreenMode = enum(u8) {
+    Borderless, // A fullScreen mode that simply resize the window.
+    Exclusive, // A fullScreen mode with change in the display's video mode.
+};
+
+pub const WindowData = struct {
+    title: []const u8, // The window title.
+    video: VideoMode, // The video mode of the window.
+    position: ?WidowPoint2D, // The current Position of the top left corner of the
+    restore_point: ?WidowPoint2D, // Keeps track of where to restore the window when exiting
+    min_size: ?WidowSize, // The minimum limits of the window's size.
+    max_size: ?WidowSize, // The maximum limits of the window's size.
+    aspect_ratio: ?struct { i32, i32 }, // The (numerator,denominator) of the applied aspect ratio.
+    fullscreen_mode: ?FullScreenMode,
+    // window.
+    // cursor: WidowCursor,              // The state of the window's cursor.
+    flags: WindowFlags,
+    // input: InputState, // Both the keyboard and mouse buttons states.
+    // platform_data: platform_impl::platform_data::WindowData, // Platform specific
+};
