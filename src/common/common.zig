dcd3cbdc9cee717ee825cc65804346e26e7463e0
eddineimad0 <zerrouq.imad@outlook.com>
2023-05-18 20:06:54 +0100

    Added event namespace.

--- src/common/common.zig
+++ src/common/common.zig
@@ -2,3 +2,4 @@ pub const video_mode = @import("video_mode.zig");
 pub const geometry = @import("geometry.zig");
 pub const window_data = @import("window_data.zig");
 pub const event = @import("event.zig");
+pub const queue = @import("queue.zig");
