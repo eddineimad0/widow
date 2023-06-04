29a9e53c29381b929c4085251b94951cc9ae3059
eddineimad0 <zerrouq.imad@outlook.com>
2023-06-04 15:41:41 +0100

    Added clipboard support.

--- src/widow.zig
+++ src/widow.zig
@@ -1,7 +1,6 @@
 const std = @import("std");
 const platform = @import("platform");
-
-pub const window = @import("./window");
+const common = @import("common");
 
 pub const WidowContext = struct {
     internals: ?*platform.internals.Internals,
@@ -22,13 +21,35 @@ pub const WidowContext = struct {
         self.internals = null;
         self.is_init = false;
     }
+
+    /// returns the string contained inside the system clipboard.
+    /// # Note
+    /// This function fails if the clipboard doesn't contain a proper unicode formatted string.
+    /// The caller is responsible for freeing the returned string.
+    /// The allocator use for allocating the string is the same one passed during initialization of this WidowContext
+    /// instance.
+    pub inline fn clipboardText(self: *Self) ![:0]u8 {
+        return self.internals.?.clipboardText(self.allocator);
+    }
+
+    /// Copys the given `text` to the system clipboard.
+    pub inline fn setClipboardText(self: *Self, text: []const u8) !void {
+        return self.internals.?.setClipboardText(self.allocator, text);
+    }
 };
 
-test "should init Widow" {
+test "Should init Widow" {
     const testing = std.testing;
     var cntxt = try WidowContext.init(testing.allocator);
     defer {
         cntxt.deinit();
     }
-    std.debug.print("Widow Struct size:{}\n", .{@sizeOf(WidowContext)});
 }
+
+// Exports
+pub const window = @import("window.zig");
+pub const event = common.event;
+pub const input = common.input;
+pub const cursor = common.cursor;
+pub const geometry = common.geometry;
+pub const FullScreenMode = common.window_data.FullScreenMode;
