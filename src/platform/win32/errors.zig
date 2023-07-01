11e16b5098c7a8ba668ca3ca538d3585e5da0c7f
eddineimad0 <zerrouq.imad@outlook.com>
2023-07-01 17:55:38 +0100

    moved errors to their own file.

---
+++ src/platform/win32/errors.zig
@@ -0,0 +1,33 @@
+pub const WidowWin32Error = error{
+    FailedToRegisterWNDCLASS,
+    NtdllNotFound,
+    ProcessHandleNotFound,
+    User32DLLNotFound,
+    FailedToInitPlatform,
+};
+
+pub const ClipboardError = error{
+    FailedToOpen,
+    AccessDenied,
+    FailedToUpdate,
+    AllocationFailure,
+    OwnershipDenied,
+    FailedToRegisterViewer,
+};
+
+pub const IconError = error{
+    FailedToCreateIcon,
+    NullColorMask,
+    NullMonochromeMask,
+};
+
+pub const MonitorError = error{
+    MonitorHandleNotFound,
+    BadVideoMode,
+    MonitorNotFound,
+};
+
+pub const WindowError = error{
+    FailedToCreate,
+    NullMonitorHandle,
+};
