e39aced79accba1102c08d176fd94758200813d5
eddineimad0 <zerrouq.imad@outlook.com>
2023-07-17 16:26:12 +0100

    added an install function.

--- build.zig
+++ build.zig
@@ -1,54 +1,19 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    // common module
-    const common_module = b.createModule(.{ .source_file = .{ .path = "src/common/common.zig" } });
-
-    var platform_module: *std.build.Module = switch (target.getOs().tag) {
-        .windows => windows: {
-            var zigwin32 = b.createModule(.{
-                .source_file = .{ .path = "libs/zigwin32/win32.zig" },
-            });
-            var deps: [2]std.build.ModuleDependency = undefined;
-            deps[0] = std.build.ModuleDependency{ .name = "common", .module = common_module };
-            deps[1] = std.build.ModuleDependency{ .name = "zigwin32", .module = zigwin32 };
-            break :windows b.createModule(.{ .source_file = .{ .path = "src/platform/win32/platform.zig" }, .dependencies = &deps });
-        },
-        else => {
-            @panic("Unsupported platform");
-        },
-    };
-
-    const deps = [2]std.build.ModuleDependency{
-        std.build.ModuleDependency{ .name = "common", .module = common_module },
-        std.build.ModuleDependency{ .name = "platform", .module = platform_module },
-    };
-
-    const widow = b.createModule(.{ .source_file = .{ .path = "src/widow.zig" }, .dependencies = &deps });
-
-    const test_step = b.step("test", "Run all library tests");
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/widow.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    main_tests.addModule("common", common_module);
-    main_tests.addModule("platform", platform_module);
-    const run_main_test = b.addRunArtifact(main_tests);
-    test_step.dependOn(&run_main_test.step);
+    const widow = installWidow(b, &target);
 
     const example_step = b.step("example", "Compile example");
-
     const examples = [_][]const u8{
         "simple_window",
         "playing_with_inputs",
         "cursor_and_icon",
         "joystick",
     };
+
     for (examples) |example_name| {
         const example = b.addExecutable(.{
             .name = example_name,
@@ -64,8 +29,37 @@ pub fn build(b: *std.Build) void {
     }
 
     const all_step = b.step("all", "Build all examples and run all tests.");
-    all_step.dependOn(test_step);
     all_step.dependOn(example_step);
 
     b.default_step.dependOn(example_step);
+}
+
+fn installWidow(b: *std.Build, target: *const std.zig.CrossTarget) *std.build.Module {
+    // common module
+    const common_module = b.createModule(.{ .source_file = .{ .path = "src/common/common.zig" } });
+
+    var platform_module: *std.build.Module = switch (target.getOs().tag) {
+        .windows => windows: {
+            var zigwin32 = b.createModule(.{
+                .source_file = .{ .path = "libs/zigwin32/win32.zig" },
+            });
+            var deps: [2]std.build.ModuleDependency = undefined;
+            deps[0] = std.build.ModuleDependency{ .name = "common", .module = common_module };
+            deps[1] = std.build.ModuleDependency{ .name = "zigwin32", .module = zigwin32 };
+            break :windows b.createModule(
+                .{ .source_file = .{ .path = "src/platform/win32/platform.zig" }, .dependencies = &deps },
+            );
+        },
+        else => {
+            @panic("Unsupported platform");
+        },
+    };
+
+    const deps = [2]std.build.ModuleDependency{
+        std.build.ModuleDependency{ .name = "common", .module = common_module },
+        std.build.ModuleDependency{ .name = "platform", .module = platform_module },
+    };
+
+    const widow = b.createModule(.{ .source_file = .{ .path = "src/main.zig" }, .dependencies = &deps });
+    return widow;
 }
