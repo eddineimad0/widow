f006c77436579716b525a9a7a42d9d9689e5da01
eddineimad0 <zerrouq.imad@outlook.com>
2023-07-31 12:48:01 +0100

    added bindings to platform init and deinit functions.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 const common = @import("common");
 const cntxt = @import("context.zig");
+const platform = @import("platform");
 // Exports
 pub const geometry = common.geometry;
 pub const cursor = common.cursor;
@@ -11,3 +12,6 @@ pub const VideoMode = common.video_mode.VideoMode;
 pub const WidowContext = cntxt.WidowContext;
 pub const WindowBuilder = cntxt.WindowBuilder;
 pub const JoystickSubSystem = cntxt.JoystickSubSystem;
+
+pub const initWidowPlatform = platform.initPlatform;
+pub const deinitWidowPlatform = platform.deinitPlatform;
