08608abe6ff654f2f15b7b7c479b8b24225ff7fb
eddineimad0 <zerrouq.imad@outlook.com>
2023-08-27 20:02:30 +0200

    added x11 initialization logic.

--- src/platform/linux/xorg/global.zig
+++ src/platform/linux/xorg/global.zig
@@ -1,28 +1,46 @@
 const std = @import("std");
 const module = @import("module.zig");
 const libx11 = @import("x11/xlib.zig");
+const x11ext = @import("x11/extensions.zig");
 
 const X11Handles = struct {
     xdisplay: *libx11.Display,
     root_window: libx11.Window,
-    screen: u32,
-    xlib: ?*anyopaque,
+    default_screen: u32,
     xrandr: ?*anyopaque,
+    xinerama: ?*anyopaque,
 };
 
+// XRandRInterf
 const XRRInterface = struct {
-    const proc_XRRGetScreenResourcesCurrent = *const fn (*libx11.Display, libx11.Window) void;
-    // fields
-    XRRGetScreenResourcesCurrent: ?proc_XRRGetScreenResourcesCurrent,
+    is_v1point3: bool,
+    XRRGetScreenResourcesCurrent: x11ext.XRRGetScreenResourcesCurrentProc,
+    XRRGetScreenResources: x11ext.XRRGetScreenResourcesProc,
+    XRRFreeScreenResources: x11ext.XRRFreeScreenResourcesProc,
+    XRRGetCrtcInfo: x11ext.XRRGetCrtcInfoProc,
+    XRRFreeCrtcInfo: x11ext.XRRFreeCrtcInfoProc,
+    XRRGetOutputInfo: x11ext.XRRGetOutputInfoProc,
+    XRRFreeOutputInfo: x11ext.XRRFreeOutputInfoProc,
+    XRRGetOutputPrimary: x11ext.XRRGetOutputPrimaryProc,
+    XRRSelectInput: x11ext.XRRSelectInputProc,
+    XRRQueryVersion: x11ext.XRRQueryVersionProc,
 };
 
-const LoadedFunctions = struct {
+// XineramaIntef
+const XrmInterface = struct {
+    is_active: bool,
+    IsActive: x11ext.XineramaIsActiveProc,
+    QueryScreens: x11ext.XineramaQueryScreens,
+};
+
+const X11Extensions = struct {
     xrandr: XRRInterface,
+    xinerama: XrmInterface,
 };
 
 pub const X11Context = struct {
     handles: X11Handles,
-    functions: LoadedFunctions,
+    extensions: X11Extensions,
     var mutex: std.Thread.Mutex = std.Thread.Mutex{};
     var g_init: bool = false;
 
@@ -30,13 +48,28 @@ pub const X11Context = struct {
         .handles = X11Handles{
             .xdisplay = undefined,
             .root_window = undefined,
-            .screen = undefined,
+            .default_screen = undefined,
             .xrandr = null,
-            .xlib = null,
+            .xinerama = null,
         },
-        .functions = LoadedFunctions{
+        .extensions = X11Extensions{
             .xrandr = XRRInterface{
-                .XRRGetScreenResourcesCurrent = null,
+                .is_v1point3 = false,
+                .XRRGetCrtcInfo = undefined,
+                .XRRSelectInput = undefined,
+                .XRRFreeCrtcInfo = undefined,
+                .XRRGetOutputInfo = undefined,
+                .XRRFreeOutputInfo = undefined,
+                .XRRGetOutputPrimary = undefined,
+                .XRRGetScreenResourcesCurrent = undefined,
+                .XRRGetScreenResources = undefined,
+                .XRRFreeScreenResources = undefined,
+                .XRRQueryVersion = undefined,
+            },
+            .xinerama = XrmInterface{
+                .is_active = false,
+                .IsActive = undefined,
+                .QueryScreens = undefined,
             },
         },
     };
@@ -48,18 +81,18 @@ pub const X11Context = struct {
 
         Self.mutex.lock();
         defer mutex.unlock();
-
         if (!Self.g_init) {
             const g_instance = &Self.globl_instance;
             // Open a connection to the X server.
             g_instance.handles.xdisplay = libx11.XOpenDisplay(null) orelse {
                 return error.FailedToConnectToXServer;
             };
-            // grab the root window.
-            g_instance.handles.screen = @intCast(libx11.DefaultScreen(g_instance.handles.xdisplay));
-            g_instance.handles.root_window = libx11.RootWindow(g_instance.handles.xdisplay, g_instance.handles.screen);
+            libx11.XrmInitialize();
+            // Grab the default screen(monitor) and the root window on it.
+            g_instance.handles.default_screen = @intCast(libx11.DefaultScreen(g_instance.handles.xdisplay));
+            g_instance.handles.root_window = libx11.RootWindow(g_instance.handles.xdisplay, g_instance.handles.default_screen);
 
-            std.debug.print("[Debug]: Default screen {d}| root window {d}\n", .{ g_instance.handles.screen, g_instance.handles.root_window });
+            g_instance.loadXExtensions();
             @atomicStore(bool, &Self.g_init, true, .Release);
         }
     }
@@ -67,16 +100,70 @@ pub const X11Context = struct {
     pub fn deinitSingleton() void {
         @setCold(true);
         if (Self.g_init) {
-            Self.g_init = false;
+            @atomicStore(bool, &Self.g_init, false, .Release);
             _ = libx11.XCloseDisplay(globl_instance.handles.xdisplay);
+            globl_instance.unloadXExtensions();
         }
     }
 
-    fn loadXExtensions(self: *Self) !void {
-        _ = self;
+    fn loadXExtensions(self: *Self) void {
+        self.handles.xinerama = module.loadPosixModule("libXinerama.so.1");
+        if (self.handles.xinerama) |handle| {
+            self.extensions.xinerama.IsActive = @ptrCast(module.moduleSymbol(handle, "XineramaIsActive").?);
+            self.extensions.xinerama.QueryScreens = @ptrCast(module.moduleSymbol(
+                handle,
+                "XineramaQueryScreens",
+            ).?);
+            self.extensions.xinerama.is_active = (self.extensions.xinerama.IsActive(self.handles.xdisplay) != 0);
+        } else {
+            std.log.warn("[X11]: Xinerama library not found.\n", .{});
+        }
+
+        self.handles.xrandr = module.loadPosixModule("libXrandr.so.2");
+        if (self.handles.xrandr) |handle| {
+            self.extensions.xrandr.XRRGetCrtcInfo = @ptrCast(module.moduleSymbol(handle, "XRRGetCrtcInfo"));
+            self.extensions.xrandr.XRRFreeCrtcInfo = @ptrCast(module.moduleSymbol(handle, "XRRFreeCrtcInfo"));
+            self.extensions.xrandr.XRRGetOutputInfo = @ptrCast(module.moduleSymbol(handle, "XRRGetOutputInfo"));
+            self.extensions.xrandr.XRRSelectInput = @ptrCast(module.moduleSymbol(handle, "XRRSelectInput"));
+            self.extensions.xrandr.XRRFreeOutputInfo = @ptrCast(module.moduleSymbol(
+                handle,
+                "XRRFreeOutputInfo",
+            ));
+            self.extensions.xrandr.XRRGetOutputPrimary = @ptrCast(
+                module.moduleSymbol(handle, "XRRGetOutputPrimary"),
+            );
+            self.extensions.xrandr.XRRGetScreenResourcesCurrent = @ptrCast(
+                module.moduleSymbol(handle, "XRRGetScreenResourcesCurrent"),
+            );
+            self.extensions.xrandr.XRRGetScreenResources = @ptrCast(
+                module.moduleSymbol(handle, "XRRGetScreenResources"),
+            );
+            self.extensions.xrandr.XRRFreeScreenResources = @ptrCast(
+                module.moduleSymbol(handle, "XRRFreeScreenResources"),
+            );
+            self.extensions.xrandr.XRRQueryVersion = @ptrCast(
+                module.moduleSymbol(handle, "XRRQueryVersion"),
+            );
+            var minor: i32 = 0;
+            var major: i32 = 0;
+            _ = self.extensions.xrandr.XRRQueryVersion(self.handles.xdisplay, &major, &minor);
+            self.extensions.xrandr.is_v1point3 = (major >= 1 and minor >= 5);
+        } else {
+            std.log.warn("[X11]: XRandR library not found.\n", .{});
+        }
     }
 
-    // fn freeLibraries(_: *Self) void {}
+    fn unloadXExtensions(self: *Self) void {
+        if (self.handles.xinerama) |handle| {
+            module.freePosixModule(handle);
+            self.handles.xinerama = null;
+        }
+
+        if (self.handles.xrandr) |handle| {
+            module.freePosixModule(handle);
+            self.handles.xrandr = null;
+        }
+    }
 
     // Enfoce readonly.
     pub fn singleton() *const Self {
@@ -107,9 +194,9 @@ pub const X11Context = struct {
 //     }
 // }
 
-test "Win32Context init" {
-    const testing = std.testing;
-    const singleton = X11Context.singleton();
-    try testing.expect(singleton != null);
-    std.debug.print("Win32 execution context: {any}\n", .{singleton.?});
-}
+// test "Win32Context init" {
+//     try X11Context.initSingleton();
+//     const singleton = X11Context.singleton();
+//     std.debug.print("Win32 execution context: {any}\n", .{singleton});
+//     X11Context.deinitSingleton();
+// }
--- src/platform/linux/xorg/platform.zig
+++ src/platform/linux/xorg/platform.zig
@@ -1,9 +1,11 @@
 const std = @import("std");
 const X11Context = @import("global.zig").X11Context;
+pub const internals = @import("internals.zig");
+pub const window_impl = @import("window_impl.zig");
+const monitor_impl = @import("monitor_impl.zig");
 
 pub fn initPlatform(options: anytype) !void {
     _ = options;
-    std.debug.print("X11 init\n", .{});
     try X11Context.initSingleton();
 }
 
