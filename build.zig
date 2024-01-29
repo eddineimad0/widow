91a9149cb407bd29bce15a3deefbd8988c1d7dd3
eddineimad0 <61315998+eddineimad0@users.noreply.github.com>
2024-01-29 01:15:49 +0100

    update build.zig

--- build.zig
+++ build.zig
@@ -27,9 +27,9 @@ pub fn build(b: *std.Build) !void {
 
     const example_step = b.step("example", "Compile example");
     const examples = [_][]const u8{
-        "simple_window",
-        "playing_with_inputs",
-        "cursor_and_icon",
+        // "simple_window",
+        // "playing_with_inputs",
+        // "cursor_and_icon",
         "xorg_basic",
     };
     for (examples) |example_name| {
