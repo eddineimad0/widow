69995a27e8dfac465897aec93b9f5d3a5df7cebb
eddineimad0 <imadzerrouq@proton.me>
2024-07-01 23:22:48 +0100

    test: timer unit test.

--- src/timer.zig
+++ src/timer.zig
@@ -21,3 +21,19 @@ pub const Timer = struct {
             @as(f64, @floatFromInt(self.freq));
     }
 };
+
+test "Timer API" {
+    const std = @import("std");
+    const testing = std.testing;
+    const t = Timer.init();
+
+    const initial_ticks = t.ticksCount();
+    while (t.now() < 3.0) {
+        asm volatile ("nop");
+    }
+    const elapsed_ticks = t.ticksCount() - initial_ticks;
+    const elapsed = @as(f64, @floatFromInt(elapsed_ticks)) /
+        @as(f64, @floatFromInt(t.freq));
+
+    try testing.expectApproxEqRel(elapsed, @as(f64, 3.0), std.math.floatEps(f64));
+}
