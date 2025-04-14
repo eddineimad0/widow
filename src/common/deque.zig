6f77af4ba13dbd58a176e50dba6c8b07f4b140e0
eddineimad0 <61315998+eddineimad0@users.noreply.github.com>
2025-04-14 18:56:16 +0200

    add unit tests

--- src/common/deque.zig
+++ src/common/deque.zig
@@ -7,13 +7,13 @@ const dbg = std.debug;
 pub fn Deque(comptime T: type) type {
     return struct {
         data: []T,
-        size:usize,
-        front:usize,
+        size: usize,
+        front: usize,
 
         const Self = @This();
 
         /// create a new `Deque`, the allocator is used to allocate the backing buffer.
-       /// `deinit()` should be called to free the buffer.
+        /// `deinit()` should be called to free the buffer.
         pub fn init(
             allocator: mem.Allocator,
             capacity: usize,
@@ -23,9 +23,9 @@ pub fn Deque(comptime T: type) type {
             }
 
             const final_capacity = if (((capacity - 1) & capacity) == 0)
-                math.ceilPowerOfTwoAssert(usize, capacity)
+                capacity
             else
-                capacity;
+                math.ceilPowerOfTwoAssert(usize, capacity);
 
             dbg.assert(capacity > 0);
             const data = try allocator.alloc(T, final_capacity);
@@ -44,25 +44,27 @@ pub fn Deque(comptime T: type) type {
             self.* = undefined;
         }
 
-        pub inline fn writeIndex(self: *const Self) usize {
+        inline fn writeIndex(self: *const Self) usize {
             return (self.front + self.size) % self.data.len;
         }
 
-        pub fn doubleCapacity(self:*Self,a:mem.Allocator)mem.Allocator.Error!void{
+        //WARN: Shouldn't be called if the dequeue isn't full since it doesn't
+        // handle all cases, so keep it private and watch how you use it.
+        fn doubleCapacity(self: *Self, a: mem.Allocator) mem.Allocator.Error!void {
+            dbg.assert(self.isFull());
             const new_capacity = self.data.len * 2;
             const new_buffer = try a.alloc(T, new_capacity);
-            if(self.writeIndex() >= self.front){
-                @memcpy(new_buffer, self.data[self.front..]);
-            }else{
-                @memcpy(new_buffer, self.data[self.front..]);
-                const amount_copied = self.data.len - self.front;
-                @memcpy(new_buffer[amount_copied..], self.data[0..self.writeIndex()]);
-            }
+            const first_copy_size = self.data.len - self.front;
+            @memcpy(new_buffer[0..first_copy_size], self.data[self.front..]);
+            @memcpy(
+                new_buffer[first_copy_size..(first_copy_size + self.writeIndex())],
+                self.data[0..self.writeIndex()],
+            );
+
             a.free(self.data);
             self.data = new_buffer;
             self.front = 0;
         }
-
 
         /// Returns `true` if the queue is empty and `false` otherwise.
         pub inline fn isEmpty(self: *const Self) bool {
@@ -74,8 +76,8 @@ pub fn Deque(comptime T: type) type {
             return self.size == self.data.len;
         }
 
-        pub fn pushBack(self:*Self,a:mem.Allocator,item:*const T)mem.Allocator.Error!void{
-            if(self.isFull()){
+        pub fn pushBack(self: *Self, a: mem.Allocator, item: *const T) mem.Allocator.Error!void {
+            if (self.isFull()) {
                 try self.doubleCapacity(a);
             }
 
@@ -83,8 +85,8 @@ pub fn Deque(comptime T: type) type {
             self.size += 1;
         }
 
-        pub fn popFront(self:*Self, output:*T)bool{
-            if(self.isEmpty()){
+        pub fn popFront(self: *Self, output: *T) bool {
+            if (self.isEmpty()) {
                 return false;
             }
 
@@ -98,7 +100,177 @@ pub fn Deque(comptime T: type) type {
     };
 }
 
+test "deque_create_destory" {
+    const testing = std.testing;
+    var d = try Deque(u8).init(testing.allocator, 10);
+    defer d.deinit(testing.allocator);
+    try testing.expectEqual(d.data.len, 16);
+    try testing.expectEqual(d.size, 0);
+    try testing.expectEqual(d.front, 0);
+    try testing.expect(d.isEmpty());
 
-test "deque" {
-    //TODO: add unit tests.
+    try testing.expectError(error.CapacityZero, Deque(u8).init(testing.allocator, 0));
+}
+
+test "deque_pushBack_popFront" {
+    const testing = std.testing;
+    var d = try Deque(u8).init(testing.allocator, 5);
+    defer d.deinit(testing.allocator);
+    try d.pushBack(testing.allocator, &10);
+    try testing.expect(!d.isEmpty());
+    try testing.expectEqual(d.size, 1);
+    var data: u8 = 0;
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 10);
+    try testing.expect(d.isEmpty());
+    try testing.expect(!d.isFull());
+    try testing.expectEqual(d.size, 0);
+
+    try d.pushBack(testing.allocator, &20);
+    try d.pushBack(testing.allocator, &30);
+    try testing.expectEqual(d.size, 2);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 20);
+    try testing.expectEqual(d.size, 1);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 30);
+    try testing.expectEqual(d.size, 0);
+    try testing.expect(d.isEmpty());
+    try testing.expect(!d.isFull());
+}
+
+test "deque_mixed_operations" {
+    const testing = std.testing;
+    var d = try Deque(i32).init(testing.allocator, 4);
+    defer d.deinit(testing.allocator);
+
+    try d.pushBack(testing.allocator, &-1);
+    try d.pushBack(testing.allocator, &0);
+    try d.pushBack(testing.allocator, &1);
+    try d.pushBack(testing.allocator, &2); // [-1, 0, 1, 2] - Full
+    try testing.expectEqual(d.size, 4);
+    try testing.expect(d.isFull());
+    try testing.expect(!d.isEmpty());
+    var data: i32 = 0;
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, -1);
+    try testing.expectEqual(d.size, 3);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 0);
+    try testing.expectEqual(d.size, 2);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 1);
+    try testing.expectEqual(d.size, 1);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 2);
+    try testing.expectEqual(d.size, 0);
+}
+
+test "deque_wrap_around" {
+    const testing = std.testing;
+    var d = try Deque(i32).init(testing.allocator, 4);
+    defer d.deinit(testing.allocator);
+
+    try d.pushBack(testing.allocator, &1);
+    try d.pushBack(testing.allocator, &2);
+    try d.pushBack(testing.allocator, &3); //  r        w
+    try d.pushBack(testing.allocator, &4); // [1, 2, 3, 4] - Full
+
+    var data: i32 = 0;
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 1); //    r      w
+    try testing.expectEqual(d.size, 3); // [_, 2, 3, 4]
+
+    //  w  r
+    try d.pushBack(testing.allocator, &5); // [5, 2, 3, 4] - Full
+
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 2);
+    try testing.expectEqual(d.size, 3);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 3);
+    try testing.expectEqual(d.size, 2);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 4);
+    try testing.expectEqual(d.size, 1);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 5);
+    try testing.expectEqual(d.size, 0);
+    // no more
+    try testing.expect(d.isEmpty());
+    try testing.expect(!d.isFull());
+    try testing.expect(!d.popFront(&data));
+    try testing.expectEqual(data, 5);
+    try testing.expectEqual(d.size, 0);
+
+    try d.pushBack(testing.allocator, &6); //  r     w
+    try d.pushBack(testing.allocator, &7); // [_, 6, 7, _]
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 6);
+    try testing.expectEqual(d.size, 1);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 7);
+    try testing.expectEqual(d.size, 0);
+
+    try testing.expect(d.isEmpty());
+    try testing.expect(!d.isFull());
+    try testing.expect(!d.popFront(&data));
+    try testing.expectEqual(data, 7);
+    try testing.expectEqual(d.size, 0);
+}
+
+test "deque_resize" {
+    const testing = std.testing;
+    var d = try Deque(i32).init(testing.allocator, 2);
+    defer d.deinit(testing.allocator);
+    try testing.expectEqual(d.data.len, 2);
+
+    try d.pushBack(testing.allocator, &5); //  r   w
+    try d.pushBack(testing.allocator, &10); // [5, 10] - Full
+    try testing.expectEqual(d.size, 2);
+    try testing.expect(d.isFull());
+    try testing.expect(!d.isEmpty());
+
+    // Trigger resize by pushing back
+    try d.pushBack(testing.allocator, &20); // Should resize (cap=4), becomes [5, 10, 20, X]
+    try testing.expectEqual(d.data.len, 4);
+    try testing.expectEqual(d.size, 3);
+    try testing.expect(!d.isFull());
+    try testing.expect(!d.isEmpty());
+    var data: i32 = 0;
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 5); //    r    w
+    try testing.expectEqual(d.size, 2); // [X, 10, 20, X]
+
+    // Fill it up again
+    try d.pushBack(testing.allocator, &40);
+    try d.pushBack(testing.allocator, &80); // w   r
+    try testing.expectEqual(d.size, 4); //   [80, 10, 20, 40] - Full
+    try testing.expect(d.isFull());
+    try testing.expect(!d.isEmpty());
+
+    // Trigger another resize
+    try d.pushBack(testing.allocator, &160); // Should resize (cap=8), becomes [10, 20, 40, 80, 160, X, X, X]
+    try testing.expectEqual(d.data.len, 8); //                                 r                w
+    try testing.expectEqual(d.size, 5);
+    try testing.expect(!d.isFull());
+    try testing.expect(!d.isEmpty());
+
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 10);
+    try testing.expectEqual(d.size, 4);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 20);
+    try testing.expectEqual(d.size, 3);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 40);
+    try testing.expectEqual(d.size, 2);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 80);
+    try testing.expectEqual(d.size, 1);
+    try testing.expect(d.popFront(&data));
+    try testing.expectEqual(data, 160);
+    try testing.expectEqual(d.size, 0);
+    try testing.expect(d.isEmpty());
+    try testing.expect(!d.isFull());
 }
