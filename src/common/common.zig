0d78c61a86eb69ab3f487441b4f07c89764d7d3c
eddineimad0 <61315998+eddineimad0@users.noreply.github.com>
2025-04-18 19:22:41 +0200

    trying to add unit tests to build script...such a pain

--- src/common/common.zig
+++ src/common/common.zig
@@ -2,7 +2,6 @@ pub const video_mode = @import("video_mode.zig");
 pub const geometry = @import("geometry.zig");
 pub const window_data = @import("window_data.zig");
 pub const event = @import("event.zig");
-pub const queue = @import("queue.zig");
 pub const keyboard_mouse = @import("keyboard_mouse.zig");
 pub const cursor = @import("cursor.zig");
 pub const fb = @import("framebuffer.zig");
@@ -10,3 +9,17 @@ pub const fb = @import("framebuffer.zig");
 const builtin = @import("builtin");
 pub const unix = if (builtin.target.os.tag == .windows) {} else @import("unix/unix.zig");
 pub const IS_DEBUG_BUILD = builtin.mode == .Debug;
+
+const queue = @import("queue.zig");
+const deque = @import("deque.zig");
+
+
+test "common_mod_tests" {
+    const std = @import("std");
+    const testing = std.testing;
+
+    testing.refAllDeclsRecursive(queue);
+    testing.refAllDeclsRecursive(deque);
+
+    try testing.expect(false);
+}
